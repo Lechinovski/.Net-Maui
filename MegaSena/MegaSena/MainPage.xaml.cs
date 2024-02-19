df32b74bb61efc88e72143629ef5b6bdc1065e32
João Lechinovski <98363221+Lechinovski@users.noreply.github.com>
2024-02-18 23:21:45 -0300

    App MegaSena
    app para sortear 6 números para jogar na megasena

---
+++ MegaSena/MegaSena/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace MegaSena;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnGerarNumbers(object sender, EventArgs e)
+    {
+        Gerar.IsVisible = false;
+        Gerado.IsVisible = true;
+
+        var set = GerarNumbers();
+
+        Num01.Text = set.ElementAt(0).ToString("D2");
+        Num02.Text = set.ElementAt(1).ToString("D2");
+        Num03.Text = set.ElementAt(2).ToString("D2");
+        Num04.Text = set.ElementAt(3).ToString("D2");
+        Num05.Text = set.ElementAt(4).ToString("D2");
+        Num06.Text = set.ElementAt(5).ToString("D2");
+    }
+
+    private SortedSet<int> GerarNumbers()
+    {
+        var set = new SortedSet<int>();
+
+        while (set.Count < 6)
+        {
+
+            var random = new Random();
+            var luckNumber = random.Next(1, 60);
+
+            set.Add(luckNumber);
+        }
+
+        return set;
+
+    }
+}
